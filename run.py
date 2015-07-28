08fade5f1949ee5c730c5c78a80b9423244c1da6
Rizki Perdana <rrangkuti@traveloka.com>
2015-07-28 17:23:34 +0700

    test edit

--- run.py
+++ run.py
@@ -27,18 +27,6 @@ def index():
 def jsontest():
 	return jsonify(username='Rizki',password='test')
 
-@app.route('/shalat/<int:day>')
-def shalat(day):
-	halaman = scrap("http://jadwalsholat.pkpu.or.id/")
-	tabel = halaman.find('table',{'class':'table_adzan'})
-	records = tabel.findAll('tr')
-	headers = [fieldName.text for fieldName in records[3].findAll('td')]
-	contents = [ [cell.text for cell in row.findAll('td')] for row in records[4:]]
-	fullContents = dict()
-	for content in contents:
-	    fullContents[content[0]]=dict(zip(headers,content))
-	return jsonify(fullContents[parseDate(day)])
-	
 	
 
 if(__name__=="__main__"):
