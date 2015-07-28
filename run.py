19219ce9e9a5e9974da39cfca4dbb5f7de9731c7
Rizki Perdana <rrangkuti@traveloka.com>
2015-07-28 18:54:25 +0700

    test lagi

--- run.py
+++ run.py
@@ -1,5 +1,21 @@
 from flask import Flask, jsonify,request
+from bs4 import BeautifulSoup
+import urllib2
 
+
+def scrap(url):
+        html = urllib2.urlopen(url).read()
+        scrapper = BeautifulSoup(html,"html.parser")
+        return scrapper
+
+def parseDate(date):
+	if(date>=10):
+		return '{0}'.format(date)
+	elif(date>=0 or date<=10):
+		return '0{0}'.format(date)
+	else: 
+		return '{0}'.format(date)
+	
 
 app = Flask(__name__)
 
@@ -11,6 +27,21 @@ def index():
 def jsontest():
 	return jsonify(username='Rizki',password='test')
 
+@app.route('/shalat/<int:day>')
+def shalat(day):
+	halaman = scrap("http://jadwalsholat.pkpu.or.id/")
+	tabel = halaman.find('table',{'class':'table_adzan'})
+	records = tabel.findAll('tr')
+	headers = [fieldName.text for fieldName in records[3].findAll('td')]
+	contents = [ [cell.text for cell in row.findAll('td')] for row in records[4:]]
+	fullContents = dict()
+	for content in contents:
+	    fullContents[content[0]]=dict(zip(headers,content))
+	return jsonify(fullContents[parseDate(day)])
+	
+	
 
 if(__name__=="__main__"):
-	app.run(debug=True, port=33507)
+	app.run()
+
+
