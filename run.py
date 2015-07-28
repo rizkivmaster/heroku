0d01fa977fdabffbb1019bbf1764b20783e62631
Rizki Perdana <rrangkuti@traveloka.com>
2015-07-28 18:32:26 +0700

    coba lagi

--- run.py
+++ run.py
@@ -1,16 +1,6 @@
 from flask import Flask, jsonify,request
 
 
-
-def parseDate(date):
-	if(date>=10):
-		return '{0}'.format(date)
-	elif(date>=0 or date<=10):
-		return '0{0}'.format(date)
-	else: 
-		return '{0}'.format(date)
-	
-
 app = Flask(__name__)
 
 @app.route('/')
