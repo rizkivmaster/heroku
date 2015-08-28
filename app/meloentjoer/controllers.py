00d81195a3a1f7b1c93c10a98388389bca73f625
rizkivmaster <rizkivmaster@gmail.com>
2015-08-29 03:13:48 +0700

    rebrand meloentjoer

--- app/meloentjoer/controllers.py
+++ app/meloentjoer/controllers.py
@@ -72,13 +72,12 @@ for table in tables:
         name = rowParser(row.find_all('td')[1].getText())
         stationSet.add(title+'_'+name)
         stationMap[title+'_'+name]=(title,name)
-        points.append(title+' '+name)
-        suggestionList.append((title+' '+name,name))
-        suggestionList.append((title+' '+name,name.lower()))
-        suggestionList.append((title+' '+name,title+' '+name))
-        suggestionList.append((title+' '+name,(title+' '+name).lower()))
+        #name important
+        points.append('Halte '+name)
+        suggestionList.append(('Halte '+name,name))
+        suggestionList.append(('Halte '+name,name.lower()))
 #         print(title+'_'+name)
-    trayek.extend(map(lambda x: '{0},{1},{2},1000,20'.format(x[0],x[1],title),zip(points[:-1],points[1:])))
+    trayek.extend(map(lambda x: '{0},{1},{2},0,10'.format(x[0],x[1],title),zip(points[:-1],points[1:])))
     
 #     train transfer lines
 for table in tables:
@@ -94,7 +93,8 @@ for table in tables:
         transferSet,transferMap = multiHrefParser(row.find_all('td')[2])
         for transferKey in transferSet:
             if(text in transferMap[transferKey][1]):
-                trayek.append('{0} {1},{2} {3},Jalan Kaki,0,10'.format(title,text,transferMap[transferKey][0],transferMap[transferKey][1]))    
+                pass
+                #trayek.append('Halte {1} {0},Halte {3} {2},Transfer,0,120'.format(title,text,transferMap[transferKey][0],transferMap[transferKey][1]))
 #                 print('Transfer : from {0}_{1} to {2}_{3}'.format(title,text,transferMap[transferKey][0],transferMap[transferKey][1]))
 #           train station nearbies          
 for table in tables:
@@ -113,12 +113,15 @@ for table in tables:
                 cleanedNearby = re.sub('Bus Terminal','',nearbyMap[key][1])
                 cleanedNearby = re.sub('Station','',cleanedNearby)
                 cleanedNearby = cleanedNearby.strip()
-                suggestionList.append((nearbyMap[key][0]+' '+cleanedNearby,cleanedNearby))
-                suggestionList.append((nearbyMap[key][0]+' '+cleanedNearby,cleanedNearby.lower()))
-                suggestionList.append((nearbyMap[key][0]+' '+cleanedNearby,nearbyMap[key][0]+' '+cleanedNearby))
-                suggestionList.append((nearbyMap[key][0]+' '+cleanedNearby,(nearbyMap[key][0]+' '+cleanedNearby).lower()))
+                nextTitle=nearbyMap[key][0]
+                if(nearbyMap[key][0]=='Bus Terminal'):
+                    nextTitle='Terminal'
+                if(nearbyMap[key][0]=='Train Station'):
+                    nextTitle='Stasiun'
+                suggestionList.append((nextTitle+' '+cleanedNearby,cleanedNearby))
+                suggestionList.append((nextTitle+' '+cleanedNearby,cleanedNearby.lower()))
 #                 print('{0}_{1} is near to {2}_{3}'.format(title,text,nearbyMap[key][0],cleanedNearby))
-                trayek.append('{3} {0},{1} {2},Jalan Kaki,0,10'.format(text,nearbyMap[key][0],cleanedNearby,title))
+                trayek.append('Halte {0},{1} {2},Jalan Kaki,0,4'.format(text,nextTitle,cleanedNearby,title))
         except Exception,err:
             pass
 
@@ -133,18 +136,16 @@ waypointMap = dict()
 for ka in kascrapper.select('dl > dd > b'):
     line = re.findall('[a-zA-Z0-9 ]+[a-zA-Z0-9]',ka.parent.parent.previousSibling.previousSibling.string)[0].strip()
     waypoint = LinkedHash(map(lambda x:x.strip(),re.sub(u'\u2192',',',re.sub('\\.','',ka.parent.getText())).split(',')))
-    suggestionList.extend(map(lambda x:('Train Station '+x,x),waypoint))
-    suggestionList.extend(map(lambda x:('Train Station '+x,x.lower()),waypoint))
-    suggestionList.extend(map(lambda x:('Train Station '+x,'Train Station '+x),waypoint))
-    suggestionList.extend(map(lambda x:('Train Station '+x,('Train Station '+x).lower()),waypoint))
+    suggestionList.extend(map(lambda x:('Stasiun '+x,x),waypoint))
+    suggestionList.extend(map(lambda x:('Stasiun '+x,x.lower()),waypoint))
     waypointMap[line] = waypoint
 for key in waypointMap.keys():
     waypoint = waypointMap[key]
-    trayek.extend(map(lambda x:'Train Station {0},Train Station {1},Commuter {2},1000,20'.format(x[0],x[1],key),zip(waypoint[1:],waypoint[:-1])))
+    trayek.extend(map(lambda x:'Stasiun {0},Stasiun {1},KRL {2},500,7'.format(x[0],x[1],key),zip(waypoint[1:],waypoint[:-1])))
     
-# print('=================================TRAYEK==================================')
-# for tray in trayek:
-#     print(tray)
+print('=================================TRAYEK==================================')
+for tray in trayek:
+    print(tray)
     
 class TrieNode:
     def __init__(self):
@@ -314,30 +315,54 @@ def find(graph, source):
                             #debug
 #                             for modeX in newModeList:
 #                                 print('     '+str(modeX))
-                        tempMap = dict()
-                        tempList = list()
-                        for trx in transport[nextCity]:
-                            tempMap[str(trx[0])+str(trx[1])]=trx
-                        for key in tempMap.keys():
-                            tempList.append(tempMap[key])
-                        tempList.sort(cmp=lambda x,y: x[0]-y[0])
-                        transport[nextCity] = tempList[:3]
+                    tempMap = dict()
+                    tempList = list()
+                    for trx in transport[nextCity]:
+                        tempMap['_'.join(x.origin+'_'+x.destination for x in trx[2])]=trx
+                    for key in tempMap.keys():
+                        tempList.append(tempMap[key])
+                    tempList.sort(cmp=lambda x,y: x[0]-y[0])
+                    transport[nextCity] = tempList[:3]
     return cost,previous,transport
+
+class ResultWrapper:
+    def __init__(self,head,trail):
+        self.head = head
+        self.trail = trail
 
 def getDirection(source,destination):
     result = list()
     cost,previous,transport = find(graph,source)
-    for key in [destination]:
-        trans = transport[key]
-        for tran in trans:
-            temp = list()
-            cost = tran[0]
-            price = tran[1]
-            modeList = tran[2]
-            temp.append('cost time for {0} is {1} with price {2}'.format(key,cost,price))
-            for mode in modeList:
-                temp.append('     '+str(mode))
-            result.append(temp)
+    trans = transport[destination]
+    diffSet = set()
+    for tran in trans:
+        allSet = set()
+        modeList = tran[2]
+        for mode in modeList:
+            allSet.add(mode.origin)
+        if(len(diffSet)==0):
+            diffSet = allSet
+        diffSet = allSet.intersection(diffSet)
+
+    for tran in trans:
+        time = tran[0]
+        price = tran[1]
+        modeList = tran[2]
+        testSet = set()
+        # temp.append('cost time for {0} is {1} with price {2}'.format(key,cost,price))
+        for mode in modeList:
+            if('TransJakarta' in mode.trnsName):
+                mode.origin = re.sub('TransJakarta Corridor.*','',mode.origin)
+                mode.destination = re.sub('TransJakarta Corridor.*','',mode.destination)
+            testSet.add(mode.origin)
+        diff = testSet - diffSet
+        viaStation = None
+        if(len(diff)==0):
+            viaStation = None
+        else:
+            viaStation = list(diff)[0]
+        resultitem = ResultWrapper((time,price,len(modeList),source,destination,viaStation),modeList)
+        result.append(resultitem)
     return result
     
 def retrieveSuggestion(node,word):
