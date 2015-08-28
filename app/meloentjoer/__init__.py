cc9eff2c6ccdff51ba4dca5957250d4a4dbb4e85
rizkivmaster <rizkivmaster@gmail.com>
2015-08-29 03:13:48 +0700

    rebrand meloentjoer

--- app/meloentjoer/__init__.py
+++ app/meloentjoer/__init__.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, jsonify,request
 from controllers import retrieveSuggestion,node,getDirection
 import json
+import os
+	
+host_url = os.environ['HOST_URL']
 
 meloentjoer = Blueprint('meloentjoer',__name__)
 @meloentjoer.route('/retrieve/<string:word>')
@@ -8,14 +11,23 @@ def get(word):
 	wordList = retrieveSuggestion(node,word)
 	return json.dumps(wordList)
 
-# @meloentjoer.route('/search')
+
+
+# @meloentjoer.route('/search',methods=['POST'])
+# def search():
+# 	json = request.get_json()
+# 	source = json['source']
+# 	destination = json['destination']
+# 	return jsonify(data=getDirection(source,destination))
+
 @meloentjoer.route('/search',methods=['POST'])
 def search():
 	json = request.get_json()
 	source = json['source']
 	destination = json['destination']
-	return jsonify(data=getDirection(source,destination))
+	data=getDirection(source,destination)
+	return jsonify(data=render_template('response.html',entries=data))
 
 @meloentjoer.route('/')
 def index():
-	return render_template('autocomplete.html')
+	return render_template('autocomplete.html',host_url=host_url)
